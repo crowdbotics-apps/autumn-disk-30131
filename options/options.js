92176049b49c1bb44f92c8775cf074e3ad2336e1
Crowdbotics <team@crowdbotics.com>
2021-08-26 17:28:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "autumn_disk_30131Identifier",
+  url: "https://autumn_disk_30131Identifier.botics.co",
+  api: "https://autumn_disk_30131Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
